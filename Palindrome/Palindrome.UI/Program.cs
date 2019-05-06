e79b5e6dbe4308a58de4ebd64d89925a0fdc5f2b
msunnypriyanka@gmail.com <msunnypriyanka@gmail.com>
2019-05-06 11:33:59 -0500

    add sql

---
+++ Palindrome/Palindrome.UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindrome.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string a = Console.ReadLine();
+            string s, revs = "";
+            Console.WriteLine("Enter string");
+            s= Console.ReadLine();
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                revs = revs + s[i].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not Palindrome");
+
+            }
+            //Console.ReadKey();
+
+        }
+    }
+}
